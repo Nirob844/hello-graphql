fa28926b35438203b3997b5e85ef5a7b4229aefa
Nirob Hasan <nirob427417@gmail.com>
2024-05-21 16:35:00 +0600

    products categories and reviews

---
+++ dist/gql/resolvers/index.js
@@ -0,0 +1,27 @@
+import { db } from "../../db.js";
+export const resolvers = {
+    Query: {
+        products: () => db.products,
+        product: (parent, args, context) => {
+            const result = db.products.find((pd) => pd.id === args.productId);
+            return result;
+        },
+        categories: () => db.categories,
+        category: (parent, args, context) => {
+            return db.categories.find((category) => category.id === args.categoryId);
+        },
+    },
+    Product: {
+        category: ({ categoryId }, args, context) => {
+            return db.categories.find((category) => category.id === categoryId);
+        },
+        reviews: ({ id }, args, context) => {
+            return db.reviews.filter((review) => review.productId === id);
+        },
+    },
+    Category: {
+        products: ({ id }, args, context) => {
+            return db.products.filter((product) => product.categoryId === id);
+        },
+    },
+};
--- src/gql/resolvers/index.ts
+++ src/gql/resolvers/index.ts
@@ -7,5 +7,22 @@ export const resolvers = {
       const result = db.products.find((pd) => pd.id === args.productId);
       return result;
     },
+    categories: () => db.categories,
+    category: (parent: any, args: { categoryId: string }, context) => {
+      return db.categories.find((category) => category.id === args.categoryId);
+    },
+  },
+  Product: {
+    category: ({ categoryId }, args: any, context: any) => {
+      return db.categories.find((category) => category.id === categoryId);
+    },
+    reviews: ({ id }, args: any, context: any) => {
+      return db.reviews.filter((review) => review.productId === id);
+    },
+  },
+  Category: {
+    products: ({ id }, args: any, context: any) => {
+      return db.products.filter((product) => product.categoryId === id);
+    },
   },
 };
